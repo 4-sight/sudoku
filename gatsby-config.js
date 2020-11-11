107a4523bda08fe4fe1e06800997c1b970e5cf58
Peter Newton <peter@4sightdesigns.com>
2020-11-11 11:16:11 +0000

    Add plugins needed for netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    {
+      resolve: `gatsby-transformer-json`,
+      options: {
+        typeName: ({ node, object, isArray }) => {
+          // console.info("Node: ", node)
+          return node.relativePath.split("/")[0]
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/`,
+      },
+    },
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.ts`,
+      },
+    },
+  ],
 }
