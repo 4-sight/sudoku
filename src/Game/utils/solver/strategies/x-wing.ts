e4359641a55c196fcebcd41134156e63baab791b
Peter Newton <peter@4sightdesigns.com>
2020-11-18 11:51:19 +0000

    Refactor

--- src/Game/utils/solver/strategies/x-wing.ts
+++ src/Game/utils/solver/strategies/x-wing.ts
@@ -2,44 +2,53 @@ import Grid from "../Classes/Grid"
 import GridArea from "../Classes/GridArea"
 import GridCell from "../Classes/GridCell"
 
-const xWing = (grid: Grid) => {
+const xWing = (grid: Grid): boolean => {
   // Search grid
   return [grid.rows, grid.cols].some(areaGroup =>
     // for rows/ cols
     areaGroup.areas.some((area, i) =>
       // for each grid/row find values that can only be in 2 positions (in different boxes, same box pairs are handled by hidden sets)
-      findDoubles(area).some(([double, value]) => {
+      area.getNPositions(2, { sameBoxAny: true }).some(([double, value]) => {
         // Search each subsequent parallel area for the same value present in only 2 positions perpendicular to the first 2
-        return findX(double, value, areaGroup.areas, i)
+        const pAxis: "row" | "col" = area.type === "row" ? "col" : "row"
+        const { wing1, wing2 } = findX(double, value, areaGroup.areas, i, pAxis)
+
+        if (wing1 && wing2) {
+          // Remove possible positions on parallel axes
+          const triggers = [...wing1, ...wing2].map(cell => cell.index)
+          const axis1Updated = wing1[0][pAxis].removeFromAllExcept(
+            wing1,
+            [value],
+            "x-wing",
+            triggers
+          )
+
+          const axis2Updated = wing2[0][pAxis].removeFromAllExcept(
+            wing2,
+            [value],
+            "x-wing",
+            triggers
+          )
+
+          // If possibilities were removed
+          if (axis1Updated || axis2Updated) {
+            return true
+          }
+        }
       })
     )
   )
 }
 
-export const findDoubles = (
-  area: GridArea
-): [[GridCell, GridCell], number][] => {
-  const doubles: [[GridCell, GridCell], number][] = []
-
-  area.pos.forEach((positions, value) => {
-    if (positions.size === 2) {
-      const cells = positions.toArray() as [GridCell, GridCell]
-      if (cells[0].box !== cells[1].box) {
-        doubles.push([cells, value])
-      }
-    }
-  })
-
-  return doubles
-}
-
 export const findX = (
-  double: [GridCell, GridCell],
+  double: GridCell[],
   value: number,
   areas: GridArea[],
-  index: number
-): boolean => {
-  let updated = false
+  index: number,
+  pAxis: "row" | "col"
+): { wing1: null | GridCell[]; wing2: null | GridCell[] } => {
+  let wing1: [GridCell, GridCell] | null = null
+  let wing2: [GridCell, GridCell] | null = null
 
   for (let i = index + 1; i < areas.length; i++) {
     const nextArea = areas[i]
@@ -50,9 +59,6 @@ export const findX = (
     // If there are 2 possible positions
     if (possiblePositions && possiblePositions.size === 2) {
       const [posCell0, posCell1] = possiblePositions.toArray()
-      const pAxis: "row" | "col" = nextArea.type === "row" ? "col" : "row"
-      let wing1: [GridCell, GridCell] | null = null
-      let wing2: [GridCell, GridCell] | null = null
 
       const boxes = new Set([
         double[0].box,
@@ -80,34 +86,14 @@ export const findX = (
           wing1 = [double[0], posCell1]
           wing2 = [double[1], posCell0]
         }
-
-        if (wing1 && wing2) {
-          // Remove possible positions on parallel axes
-          const triggers = [...wing1, ...wing2].map(cell => cell.index)
-          const axis1Updated = wing1[0][pAxis].removeFromAllExcept(
-            wing1,
-            [value],
-            "x-wing",
-            triggers
-          )
-
-          const axis2Updated = wing2[0][pAxis].removeFromAllExcept(
-            wing2,
-            [value],
-            "x-wing",
-            triggers
-          )
-
-          // If possibilities were removed
-          if (axis1Updated || axis2Updated) {
-            updated = true
-          }
-        }
       }
     }
   }
 
-  return updated
+  return {
+    wing1,
+    wing2,
+  }
 }
 
 export default xWing
